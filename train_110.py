99c5c06a05b168ecc280f6511783b47f641ad3d2
karmueo <karmueo@163.com>
2025-10-28 14:01:18 +0800

    更新YOLOv11模型训练配置，调整超参数并恢复YOLO11n骨干网络结构

--- train_110.py
+++ train_110.py
@@ -6,11 +6,11 @@ model = YOLO(
     # "./ultralytics/cfg/models/11/yolo11m_ir_p2.yaml"
     # "./ultralytics/cfg/models/v8/yolov8m-p2p.yaml"
 ).load(
-    "runs/detect/yolov11m_110_rgb_640_nanchang_v5/weights/last.pt"
+    # "runs/detect/yolov11m_110_rgb_640_nanchang_v5/weights/last.pt"
     # "runs/detect/yolov11m_110_rgb_640_nanchang_v3/weights/last.pt"
     # "runs/detect/yolov8m_110_ir_640_nanchang/weights/last.pt"
     # "/home/tl/data/weights/yolov8m.pt"
-    # "/home/tl/data/weights/yolov11m.pt"
+    "/home/tl/data/weights/yolov11m.pt"
     # "/home/tl/data/weights/yolov11l.pt"
 )  # load a pretrained model (recommended for training)
 
@@ -27,20 +27,20 @@ results = model.train(
     cache=True,
     save_period=10,
     # single_cls=True,
-    name="yolov11m_110_rgb_640_v6",
+    name="yolov11m_110_rgb_640_v7.1",
     # name="yolov11m_110_ir_640_nanchang",
     plots=True,
-    lr0=0.001,
-    optimizer="SGD",
-    # optimizer="auto",
+    # lr0=0.001,
+    # optimizer="SGD",
+    optimizer="auto",
     amp=True,  # 自动混合精度训练
     exist_ok=True,  # 如果为true，则允许覆盖现有项目/名称目录。对于迭代实验有用，无需手动清除先前的输出
     # profile=True,  # 在训练过程中启用onnx和TensorRT速度的分析，可用于优化模型部署。
-    cls=0.8,
+    cls=0.7,
     mixup=0,
-    mosaic=0,
+    mosaic=1,
     # resume=True
-    patience=10,
+    patience=20,
     hsv_h=0.001,
     hsv_v=0.001
 )
